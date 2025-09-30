3a9ff1d929e3e176e57bb51028626162ad77b49e
BaltimoreOriole <trechenomalleyworks.inc@gmail.com>
2025-09-30 12:40:34 +0000

    thing

--- Main.java
+++ Main.java
@@ -1,1 +1,22 @@
-package com.amongus
+package com.amongus;
+//Main.java
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TrainYard newThing = new Thing("Browns Yard");
+        newThing.yardName();
+
+
+        TrainYard newType = new Type(3, "Browns Yard");
+        newType.trackYard();
+
+        Train newThis = new This("EMD GP40-2", 3);
+        newThis.trainTrack();
+
+        Train newThat = new That("EMD GP40-2", "Car Carriers and Tankers");
+        newThat.trainCargo();
+        
+    }
+}
